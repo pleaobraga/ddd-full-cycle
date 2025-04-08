7eb4d3b42b3cbe46c527792d8ea649fef216d84b
pleaobraga <pleao.braga@gmail.com>
2025-04-08 07:17:06 -0300

    feat: create order entity

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { Address } from './entity/address'
+import { Customer } from './entity/customer'
+import { Order } from './entity/order'
+import { OrderItem } from './entity/order-item'
+
+const customer = new Customer('123', 'Pedro')
+const address = new Address('Rua 1', 2, '12345-678', 'Belo Horizonte')
+customer.changeAddress(address)
+customer.activate()
+
+const item1 = new OrderItem('1', 'item 1', 10)
+const item2 = new OrderItem('2', 'item 2', 15)
+const order = new Order('1', '123', [item1, item2])
