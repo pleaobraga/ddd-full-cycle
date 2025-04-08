fc78c6792d905ee4cd77c0e94d30304eacde9276
pleaobraga <pleao.braga@gmail.com>
2025-04-08 09:13:16 -0300

    feat: create reward points test

--- src/entity/customer/customer.spec.ts
+++ src/entity/customer/customer.spec.ts
@@ -50,14 +50,14 @@ describe('Customer unit tests', () => {
     expect(customer.isActive()).toBe(false)
   })
 
-  // it('should add reward points', () => {
-  //   const customer = new Customer('1', 'Customer 1')
-  //   expect(customer.rewardPoints).toBe(0)
+  it('should add reward points', () => {
+    const customer = new Customer('1', 'Customer 1')
+    expect(customer.rewardPoints).toBe(0)
 
-  //   customer.addRewardPoints(10)
-  //   expect(customer.rewardPoints).toBe(10)
+    customer.addRewardPoints(10)
+    expect(customer.rewardPoints).toBe(10)
 
-  //   customer.addRewardPoints(10)
-  //   expect(customer.rewardPoints).toBe(20)
-  // })
+    customer.addRewardPoints(10)
+    expect(customer.rewardPoints).toBe(20)
+  })
 })
