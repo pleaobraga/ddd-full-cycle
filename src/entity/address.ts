4ef1b5d118ce24181c90964a1231ef3f2004a105
pleaobraga <pleao.braga@gmail.com>
2025-04-08 06:52:02 -0300

    feat: create address object value

---
+++ src/entity/address.ts
@@ -0,0 +1,37 @@
+export class Address {
+  _street: string = ''
+  _number: number = 0
+  _zip: string = ''
+  _city: string = ''
+
+  constructor(street: string, number: number, zip: string, city: string) {
+    this._city = city
+    this._number = number
+    this._street = street
+    this._zip = zip
+
+    this.validate()
+  }
+
+  validate() {
+    if (this._street.length === 0) {
+      throw new Error('Street is required')
+    }
+
+    if (this._number === 0) {
+      throw new Error('Number is required')
+    }
+
+    if (this._city === '') {
+      throw new Error('City is required')
+    }
+
+    if (this._zip === '') {
+      throw new Error('Zip is required')
+    }
+  }
+
+  toString() {
+    return `${this._street}, ${this._number}, ${this._zip}, ${this._city}`
+  }
+}
--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,13 +1,56 @@
+import { Address } from './address'
+
 export class Customer {
   _id: string
   _name: string
-  _address: string
+  _address!: Address
+  private _active: boolean = false
 
-  constructor(id: string, name: string, address: string) {
+  constructor(id: string, name: string) {
     this._id = id
     this._name = name
+
+    this.validate()
+  }
+
+  validate() {
+    if (this._id.length === 0) {
+      throw new Error('Id is required')
+    }
+    if (this._name.length === 0) {
+      throw new Error('Name is required')
+    }
+  }
+
+  changeName(name: string) {
+    this._name = name
+    this.validate()
+  }
+
+  get Address(): Address {
+    return this._address
+  }
+
+  changeAddress(address: Address) {
     this._address = address
   }
 
-  
+  isActive(): boolean {
+    return this._active
+  }
+
+  activate() {
+    if (this._address === undefined) {
+      throw new Error('Address is mandatory to activate a customer')
+    }
+    this._active = true
+  }
+
+  deactivate() {
+    this._active = false
+  }
+
+  set address(address: Address) {
+    this._address = address
+  }
 }
