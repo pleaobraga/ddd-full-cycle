eece05b732e92ea4d0453893448e402a7d211d22
pleaobraga <pleao.braga@gmail.com>
2025-04-09 07:23:47 -0300

    feat: create product repository

---
+++ src/domain/repository/product-repository.interface.ts
@@ -0,0 +1,4 @@
+import { Product } from '../entity/product/product'
+import { RepositoryInterface } from './repository-interface'
+
+export type ProductRepositoryInterface = RepositoryInterface<Product>
---
+++ src/infra/repository/product.repository.ts
@@ -0,0 +1,25 @@
+import { Product } from '../../domain/entity/product/product'
+import { ProductRepositoryInterface } from '../../domain/repository/product-repository.interface'
+import { ProductModel } from '../db/sequelize/model/product.model'
+
+export class ProductRepository implements ProductRepositoryInterface {
+  async create(entity: Product): Promise<void> {
+    await ProductModel.create({
+      id: entity.id,
+      name: entity.name,
+      price: entity.price,
+    })
+  }
+
+  async find(id: string): Promise<Product> {
+    throw new Error('Method not implemented')
+  }
+
+  async findAll(): Promise<Product[]> {
+    throw new Error('Method not implemented')
+  }
+
+  async update(entity: Product): Promise<void> {
+    throw new Error('Method not implemented')
+  }
+}
