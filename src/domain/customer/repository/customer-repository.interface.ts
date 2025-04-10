8dc3450558913db31785f08609d53b69551ca728
pleaobraga <pleao.braga@gmail.com>
2025-04-10 08:39:52 -0300

    feat: create customer factory

--- src/domain/customer/repository/customer-repository.interface.ts
+++ src/domain/customer/repository/customer-repository.interface.ts
@@ -1,4 +1,4 @@
-import { Customer } from '../entity/customer/customer'
-import { RepositoryInterface } from '../@shared/repository/repository-interface'
+import { RepositoryInterface } from '../../@shared/repository/repository-interface'
+import { Customer } from '../entity/customer'
 
 export type CustomerRepositoryInterface = RepositoryInterface<Customer>
